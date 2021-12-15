95f51a5f363ff706eb84edfd6ee7cf925564c415
Torben Rolfes <Lonewolff@gmx.net>
2021-12-15 12:34:57 +0100

    Solved second challenge of day06 code challenge of advent of code 2021

---
+++ day06/Perl/AoC_2021_D6_02.pl
@@ -0,0 +1,37 @@
+use strict;
+use warnings FATAL => 'all';
+
+use File::Slurp;
+use Data::Dumper;
+use List::Util;
+
+my $puzzle = File::Slurp::read_file('../Input/AoC_2021_D6.txt');
+
+my %lanternfish_by_age = ();
+
+foreach my $lanternfish_age (split(/\D/, $puzzle)) {
+	next unless $lanternfish_age;
+	++$lanternfish_by_age{$lanternfish_age};
+}
+
+my $day = 0;
+
+do {
+	print "Fishes after day $day:\t" . List::Util::sum(values %lanternfish_by_age) . "\n";
+
+	my %lanternfish_by_age_next_day = ();
+
+	foreach my $lanternfish_age (keys %lanternfish_by_age) {
+		if ($lanternfish_age > 0) {
+			$lanternfish_by_age_next_day{$lanternfish_age - 1} += $lanternfish_by_age{$lanternfish_age};
+		}
+		else {
+			$lanternfish_by_age_next_day{8} += $lanternfish_by_age{$lanternfish_age};
+			$lanternfish_by_age_next_day{6} += $lanternfish_by_age{$lanternfish_age};
+		}
+	}
+	%lanternfish_by_age = %lanternfish_by_age_next_day;
+} while (++$day < 257);
+
+print Data::Dumper::Dumper(\%lanternfish_by_age);
+
