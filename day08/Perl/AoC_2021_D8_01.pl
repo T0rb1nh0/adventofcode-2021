f22f933c7598d0b96a6971c5b4db606ca88a287e
Torben Rolfes <Lonewolff@gmx.net>
2021-12-15 22:18:24 +0100

    Solved first part of day08 challenge of advent of code 2021 coding challenges

--- day08/Perl/AoC_2021_D8_01.pl
+++ day08/Perl/AoC_2021_D8_01.pl
@@ -5,5 +5,14 @@ use File::Slurp;
 use Data::Dumper;
 use List::Util;
 
-my $puzzle = File::Slurp::read_file('../Input/AoC_2021_D8.txt');
+my @puzzle = File::Slurp::read_file('../Input/AoC_2021_D8.txt');
 
+my $counter = 0;
+foreach my $line (@puzzle) {
+    my ($signal_pattern, $output_values) = split(/\|/, $line);
+    foreach my $output_value (split(/\s/, $output_values)) {
+        ++$counter if length($output_value) > 1 && length($output_value) < 5 || length($output_value) == 7;
+    }
+}
+
+print "Number of times 1, 2, 4, 7 appears in output values:\t".$counter;
