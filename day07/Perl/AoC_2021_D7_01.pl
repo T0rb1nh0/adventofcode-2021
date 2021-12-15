fb81c2d03a133137b935a0cba1ccea525f229ffe
Torben Rolfes <Lonewolff@gmx.net>
2021-12-15 19:32:43 +0100

    Solved first part of day07 challenge of advent of code 2021 coding challenges

--- day07/Perl/AoC_2021_D7_01.pl
+++ day07/Perl/AoC_2021_D7_01.pl
@@ -7,3 +7,18 @@ use List::Util;
 
 my $puzzle = File::Slurp::read_file('../Input/AoC_2021_D7.txt');
 
+my $best_min_position = -1;
+my $best_min_fuel = 99999999;
+
+for (my $i = 0; $i < 1788; ++$i) {
+	my $current_fuel = 0;
+	foreach my $crab_position (split(/\D/, $puzzle)) {
+		$current_fuel += abs($crab_position - $i);
+	}
+	if ($current_fuel < $best_min_fuel) {
+		$best_min_position = $i;
+		$best_min_fuel = $current_fuel;
+	}
+}
+
+print "Best minimal position detected at slot $best_min_position with fuel usage of $best_min_fuel";
