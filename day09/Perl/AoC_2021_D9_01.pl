ed45a86b9d44b3688cfe0b5d8da54894d6b74548
Torben Rolfes <Lonewolff@gmx.net>
2021-12-16 15:46:43 +0100

    Solved first part of day09 challenge of advent of code 2021 coding challenges

--- day09/Perl/AoC_2021_D9_01.pl
+++ day09/Perl/AoC_2021_D9_01.pl
@@ -1,9 +1,33 @@
 use strict;
 use warnings FATAL => 'all';
+use feature 'say';
 
 use File::Slurp;
 use Data::Dumper;
 use List::Util;
 
-my $puzzle = File::Slurp::read_file('../Input/AoC_2021_D9.txt');
+my @puzzle = File::Slurp::read_file('../Input/AoC_2021_D9.txt');
 
+my @heightmap = [];
+my $y = 0;
+foreach my $line (@puzzle) {
+    my $x = 0;
+    foreach my $location (split(//, $line)) {
+        next unless $location =~ m/^\d$/;
+        $heightmap[$x++][$y] = $location;
+    }
+    ++$y;
+}
+
+my $result = 0;
+for my $y (0..$#heightmap) {
+   for my $x (0..$#{$heightmap[$y]}) {
+      next if $y > 0 && $heightmap[$y-1][$x] <= $heightmap[$y][$x];
+      next if $y < $#heightmap && $heightmap[$y+1][$x] <= $heightmap[$y][$x];
+      next if $x > 0 && $heightmap[$y][$x - 1] <= $heightmap[$y][$x];
+      next if $x < $#{$heightmap[$y]} && $heightmap[$y][$x+1] <= $heightmap[$y][$x];
+      $result += 1 +  $heightmap[$y][$x];
+   }
+}
+
+say "Sum of low points:\t".$result;
