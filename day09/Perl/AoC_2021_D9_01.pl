456b2725f15da6b6d57bcaaf0a66929e10345264
Torben Rolfes <Lonewolff@gmx.net>
2021-12-17 11:20:57 +0100

    Solved first part of day11 challenge of advent of code 2021 coding challenges

--- day09/Perl/AoC_2021_D9_01.pl
+++ day09/Perl/AoC_2021_D9_01.pl
@@ -11,23 +11,23 @@ my @puzzle = File::Slurp::read_file('../Input/AoC_2021_D9.txt');
 my @heightmap = [];
 my $y = 0;
 foreach my $line (@puzzle) {
-    my $x = 0;
-    foreach my $location (split(//, $line)) {
-        next unless $location =~ m/^\d$/;
-        $heightmap[$x++][$y] = $location;
-    }
-    ++$y;
+	my $x = 0;
+	foreach my $location (split(//, $line)) {
+		next unless $location =~ m/^\d$/;
+		$heightmap[$x++][$y] = $location;
+	}
+	++$y;
 }
 
 my $result = 0;
-for my $y (0..$#heightmap) {
-   for my $x (0..$#{$heightmap[$y]}) {
-      next if $y > 0 && $heightmap[$y-1][$x] <= $heightmap[$y][$x];
-      next if $y < $#heightmap && $heightmap[$y+1][$x] <= $heightmap[$y][$x];
-      next if $x > 0 && $heightmap[$y][$x - 1] <= $heightmap[$y][$x];
-      next if $x < $#{$heightmap[$y]} && $heightmap[$y][$x+1] <= $heightmap[$y][$x];
-      $result += 1 +  $heightmap[$y][$x];
-   }
+for my $y (0 .. $#heightmap) {
+	for my $x (0 .. $#{$heightmap[$y]}) {
+		next if $y > 0 && $heightmap[$y - 1][$x] <= $heightmap[$y][$x];
+		next if $y < $#heightmap && $heightmap[$y + 1][$x] <= $heightmap[$y][$x];
+		next if $x > 0 && $heightmap[$y][$x - 1] <= $heightmap[$y][$x];
+		next if $x < $#{$heightmap[$y]} && $heightmap[$y][$x + 1] <= $heightmap[$y][$x];
+		$result += 1 + $heightmap[$y][$x];
+	}
 }
 
-say "Sum of low points:\t".$result;
+say "Sum of low points:\t" . $result;
--- day11/Perl/AoC_2021_D11_01.pl
+++ day11/Perl/AoC_2021_D11_01.pl
@@ -1,9 +1,79 @@
 use strict;
-use warnings FATAL => 'all';
+#use warnings FATAL => 'all';
+use feature 'say';
 
 use File::Slurp;
 use Data::Dumper;
 use List::Util;
 
-my $puzzle = File::Slurp::read_file('../Input/AoC_2021_D11.txt');
+my @puzzle = File::Slurp::read_file('../Input/AoC_2021_D11.txt');
 
+my @octopusmap = [];
+my $y = 0;
+foreach my $line (@puzzle) {
+	my $x = 0;
+	foreach my $energylevel (split(//, $line)) {
+		next unless $energylevel =~ m/^\d$/;
+		$octopusmap[$x++][$y] = $energylevel;
+	}
+	++$y;
+}
+
+my $flash_counter = 0;
+for (my $steps = 1; $steps <= 100; ++$steps) {
+	for my $y (0 .. $#octopusmap) {
+		for my $x (0 .. $#{$octopusmap[$y]}) {
+			++$octopusmap[$y][$x];
+		}
+	}
+
+	my $done;
+	do {
+		$done = 1;
+
+		for my $y (0 .. $#octopusmap) {
+			for my $x (0 .. $#{$octopusmap[$y]}) {
+				if ($octopusmap[$y][$x] =~ /^\d+$/ && $octopusmap[$y][$x] > 9) {
+					$octopusmap[$y][$x] = 'F';
+					++$flash_counter;
+
+					++$octopusmap[$y - 1][$x - 1] if $x > 0 && $y > 0;
+					++$octopusmap[$y][$x - 1] if $x > 0;
+					++$octopusmap[$y - 1][$x] if $y > 0;
+
+					++$octopusmap[$y + 1][$x + 1] if $x < $#{$octopusmap[$y]} && $y < $#octopusmap;
+					++$octopusmap[$y][$x + 1] if $x < $#{$octopusmap[$y]};
+					++$octopusmap[$y + 1][$x] if $y < $#octopusmap;
+
+					++$octopusmap[$y - 1][$x + 1] if $x < $#{$octopusmap[$y]} && $y > 0;
+					++$octopusmap[$y + 1][$x - 1] if $x > 0 && $y < $#octopusmap;
+
+					$done = 0;
+				}
+			}
+		}
+	} while (!$done);
+
+	for my $y (0 .. $#octopusmap) {
+		for my $x (0 .. $#{$octopusmap[$y]}) {
+			$octopusmap[$y][$x] = 0 if $octopusmap[$y][$x] !~ /^\d+$/;
+		}
+	}
+
+	#print_array(\@octopusmap);
+}
+
+say "Total amount of flashes: " . $flash_counter;
+
+sub print_array {
+	my ($arr_ref) = @_;
+	my @arr = @$arr_ref;
+	for my $y (0 .. $#arr) {
+		for my $x (0 .. $#{$arr[$y]}) {
+			print $arr[$y][$x];
+		}
+		print "\n";
+	}
+	print "\n";
+
+}
