fb1d56762c10aec34a0b8867f08423263353483b
Torben Rolfes <Lonewolff@gmx.net>
2021-12-21 19:54:09 +0100

    Solution of first part of day17 challenge of advent of code 2021 coding challenges

--- day17/Perl/AoC_2021_D17_01.pl
+++ day17/Perl/AoC_2021_D17_01.pl
@@ -1,5 +1,6 @@
 use strict;
 use warnings FATAL => 'all';
+use feature 'say';
 
 use File::Slurp;
 use Data::Dumper;
@@ -7,3 +8,30 @@ use List::Util;
 
 my $puzzle = File::Slurp::read_file('../Input/AoC_2021_D17.txt');
 
+# Parse target area
+$puzzle =~ m/x=((?:-)?\d+)..((?:-)?\d+),\sy=((?:-)?\d+)..((?:-)?\d+)/;
+my ($min_x, $max_x, $min_y, $max_y) = ($1, $2, $3, $4);
+
+say "The target are is for x from $min_x to  $max_x and for y from $min_y to $max_y";
+
+my $highest_y_total = 0;
+foreach (my $x = 1; $x <= $max_x; ++$x) {
+	foreach (my $y = 0; $y <= abs($max_y) * 25; ++$y) {
+		my ($x_velocity, $y_velocity, $x_position, $y_position, $steps, $highest_point) = ($x, $y, 0, 0, 0, 0);
+		while (($x_velocity || $x_position >= $min_x) && $x_position <= $max_x && $y_position >= $max_y) {
+			$x_position += $x_velocity;
+			$y_position += $y_velocity;
+			--$x_velocity if $x_velocity > 0;
+			++$x_velocity if $x_velocity < 0;
+			--$y_velocity;
+			$highest_point = $y_position if $y_position > $highest_point;
+			if ($x_position >= $min_x && $x_position <= $max_x && $y_position >= $min_y && $y_position <= $max_y) {
+				say "Target area hit after $steps steps with initial velocity $x,$y. Highest point was $highest_point.";
+				$highest_y_total = $highest_point if $highest_point > $highest_y_total;
+				last;
+			}
+		}
+	}
+}
+
+say "Highest point of all tracks which hit target area is at y=$highest_y_total";
