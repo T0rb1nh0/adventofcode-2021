e0b42c23b51a0f71a3632587079ac0cd8d0a636a
Torben Rolfes <Lonewolff@gmx.net>
2021-12-25 18:14:14 +0100

    Solution of first part of day19 challenge of advent of code 2021 coding challenges

--- day19/Perl/AoC_2021_D19_01.pl
+++ day19/Perl/AoC_2021_D19_01.pl
@@ -11,12 +11,14 @@ $Data::Dumper::Sortkeys = 1;
 my @puzzle = File::Slurp::read_file('../Input/AoC_2021_D19.txt');
 
 my @scanners;
+# Load data from file
 foreach my $line (@puzzle) {
 	$line =~ s/^\s+|\s+$//g;
 	next unless $line;
 
 	if ($line =~ m/scanner (\d+)/) {
 		push(@scanners, {});
+		$scanners[-1]->{id} = $#scanners;
 	}
 	else {
 		$scanners[-1]->{beacons}->{$line} = {};
@@ -26,43 +28,51 @@ foreach my $line (@puzzle) {
 my $got_something = 1;
 while ($got_something) {
 	$got_something = 0;
-	say "Scanners left open to get merged is " . (scalar(@scanners) - 1);
 
+	say "-----------------------------------------------------------------------";
+	say "" . (@scanners - 1) . " scanners left open to get merged into scanner 0.";
+
+	# Reset fingerprints
 	foreach my $scanner (@scanners) {
 		foreach my $beacon (keys %{$scanner->{beacons}}) {
 			$scanner->{beacons}->{$beacon} = {}
 		}
 	}
 
+	# Calculate fingerprints for each beacon ( List of distances between all other beacons of the same scanner. )
 	foreach my $scanner (@scanners) {
 		foreach my $beacon1 (keys %{$scanner->{beacons}}) {
 			foreach my $beacon2 (keys %{$scanner->{beacons}}) {
-				next if $beacon1 eq $beacon2;
+				next if $beacon1 eq $beacon2; # Dont calculate distance between itself.
 				my ($m1, $n1, $o1) = split ",", $beacon1;
 				my ($m2, $n2, $o2) = split ",", $beacon2;
 				my $fp = join("#", sort {$a <=> $b} (abs($m1 - $m2), abs($n1 - $n2), abs($o1 - $o2)));
+				# Store fingerprint for both beacons
 				$scanner->{beacons}->{$beacon1}->{$fp} = $beacon2;
 				$scanner->{beacons}->{$beacon2}->{$fp} = $beacon1;
 			}
 		}
 	}
 
+	# Merge next scanner into scanner 0
 	foreach my $scanner (@scanners) {
 		next if $scanner == $scanners[0];
 
 		my %matches;
 		foreach my $beacon1 (keys %{$scanner->{beacons}}) {
 			foreach my $beacon0 (keys %{$scanners[0]->{beacons}}) {
+				# Both beacons of scanner0 and current scanner have more than ten times the same fp? They must be a match!
 				if (10 < scalar grep {$scanners[0]->{beacons}->{$beacon0}->{$_}} keys %{$scanner->{beacons}->{$beacon1}}) {
-					#say "$beacon1 matches $beacon0";
+					say "Fingerprints of beacon $beacon1 of scanner $scanner->{id} does match beacon $beacon0 of scanner 0.";
 					@{$matches{$beacon0}} = split(/,/, $beacon1);
 				}
 			}
 		}
 
+		# No matches? Lets move on because the needed beacons seems to be still part of different scanners than scanner 0.
 		next unless keys %matches;
 
-		# Check all possibilities
+		# Detect direction and rotation of scanner X compared to scanner 0 based on previously collected matches.
 		foreach my $i (1 .. 48) {
 			my %results;
 			foreach my $match (keys %matches) {
@@ -122,47 +132,69 @@ while ($got_something) {
 					$axes[2] = "-" . $axes[2];
 				}
 
+				# Beacon 0 values are always on the correct axes.
 				my ($x, $y, $z) = split(/,/, $match);
 
 				my $current_result = ($x - $m) . "," . ($y - $n) . "," . ($z - $o);
 
-				@{$results{$current_result}} = (($x - $m), ($y - $n), ($z - $o), @axes, $i);
+				@{$results{$current_result}} = (($x - $m), ($y - $n), ($z - $o), @axes);
 
-				last if keys %results > 1;
+				#say "$i: Discrepancy between scanner $scanner->{id} and scanner 0 is $current_result ($x - $m)  ($y - $n)  ($z - $o)" if $i == 26;
 			}
 
-			if (keys(%results) == 1) {
-				foreach my $result (keys(%results)) {
-					say "Position of scanner $scanner from scanner 0 perspective is $result";
-					my @vals = @{$results{$result}};
-					foreach my $beacon1 (keys %{$scanner->{beacons}}) {
-						my @beacon_val = split(/,/, $beacon1);
-						for my $i (0 .. 2) {
-							$beacon_val[$i] *= -1 if $vals[$i + 3] =~ m/^-/;
-							$beacon_val[$i] += $vals[$i];
-						}
-						my $line;
-						foreach my $axe (qw(x y z)) {
-							for my $i (0 .. 2) {
-								if ($vals[$i + 3] =~ m/$axe/) {
-									$line .= $beacon_val[$i] . ",";
-								}
+			# No results found? Nothing to get merged!
+			next if keys %results > 1;
+
+			foreach my $result (keys(%results)) {
+				my @vals = @{$results{$result}};
+
+				say "Position of scanner $scanner->{id} from scanner 0 perspective is $result with axes " . join(",", ($vals[3], $vals[4], $vals[5]));
+
+				foreach my $beacon1 (keys %{$scanner->{beacons}}) {
+
+					my @beacon_val = split(/,/, $beacon1);
+					my $col = 0;
+					my $line;
+					for my $i (3 .. 5) {
+						if ($vals[$i] =~ m/x/) {
+							my $val = $beacon_val[0];
+							if ($vals[$i] =~ m/^-/) {
+								$val *= -1;
 							}
+							$val += $vals[$col++];
+							$line .= $val . ",";
 						}
-						chop($line);
-						if ($scanners[0]->{beacons}->{$line}) {
-							#say "Beacon already detected before .. skipped ... $line";
+						elsif ($vals[$i] =~ m/y/) {
+							my $val = $beacon_val[1];
+							if ($vals[$i] =~ m/^-/) {
+								$val *= -1;
+							}
+							$val += $vals[$col++];
+							$line .= $val . ",";
 						}
-						else {
-							#say "New Beacon created at sensor 0! .... $line";
-							$scanners[0]->{beacons}->{$line} = {};
+						elsif ($vals[$i] =~ m/z/) {
+							my $val = $beacon_val[2];
+							if ($vals[$i] =~ m/^-/) {
+								$val *= -1;
+							}
+							$val += $vals[$col++];
+							$line .= $val . ",";
 						}
 					}
+					chop($line);
+					if ($scanners[0]->{beacons}->{$line}) {
+						say "Beacon $beacon1 of scanner $scanner->{id} already exists at scanner 0 as $line .. skipped";
+					}
+					else {
+						say "New Beacon $line created at scanner 0 for scanner $scanner->{id} $beacon1";
+						$scanners[0]->{beacons}->{$line} = {};
+					}
 				}
-				$got_something = 1;
-				say "Scanner $scanner removed .. Lets retry to try to merge another one.";
-				@scanners = grep {$_ != $scanner} @scanners;
 			}
+			$got_something = 1;
+			say "Scanner $scanner->{id} removed. Lets try to merge another one.";
+			@scanners = grep {$_ != $scanner} @scanners;
+			last;
 		}
 	}
 }
